7b03cea470c966be5c63503376802ceebf5501c4
apk <alexpkristiansen@icloud.com>
2024-11-05 13:04:17 -0800

    fix bug with inotify and vim

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::{fs::read_to_string, path::Path, time::Duration};
 
+use anyhow::Context;
 use axum::{extract::State, http::StatusCode, response::{sse::Event, Html, IntoResponse, Sse}, routing::get, Router};
 use html::generate;
 use markdown::Options;
-use notify::{EventKind, RecommendedWatcher, Watcher};
+use notify::{event::{DataChange, MetadataKind, ModifyKind}, EventKind, RecommendedWatcher, Watcher};
 use serde::Serialize;
 use tokio::sync::mpsc::{channel, unbounded_channel, Sender};
 use tokio_stream::Stream;
 use clap::Parser;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tracing::{debug, info};
+use tracing::{debug, error, info};
 
 mod html;
 
@@ -62,7 +63,9 @@ async fn main() -> anyhow::Result<()> {
     .route("/", get(root))
     .route("/sse/:path", get(sse_handler))
     .route("/*filename", get(link_handler))
-    .layer(tower_http::trace::TraceLayer::new_for_http())
+    .layer(
+        tower_http::trace::TraceLayer::new_for_http()
+    )
     .with_state(cli.clone());
 
     let listener = tokio::net::TcpListener::bind(cli.address).await?;
@@ -83,7 +86,7 @@ async fn sse_handler(State(state): State<Cli>, axum::extract::Path(path): axum::
 
     tokio::spawn(async move {
        if let Err(e) = file_watch(tx, newstate).await {
-        info!("error watching file, closing SSE task: {}", e);
+        error!("error watching file, closing SSE task: {}", e);
        }
     });
     
@@ -112,16 +115,17 @@ async fn root(State(state): State<Cli>) -> Result<Html<String>, BocaError> {
 
 /// blocks until the receiver closes, waits and sends file updates
 async fn file_watch(tx: Sender<Result<Event, anyhow::Error>>, opts: Cli) -> anyhow::Result<()> {
+    debug!{%opts.filename, "starting new file notify watcher"};
     //initialize with base file event
     tx.send(read_to_event(&opts.filename)).await?;
 
     let (watch_tx,mut watch_rx) = unbounded_channel::<Result<notify::Event, notify::Error>>();
-    debug!{%opts.filename, "starting new file notify watcher"};
+    
     
     let mut watcher = RecommendedWatcher::new(
         move |res: Result<notify::Event, notify::Error>| {
                 if let Err(e) = watch_tx.send(res) {
-                    debug!("error sending from watcher, exiting handler: {}", e);
+                    error!("error sending from watcher, exiting handler: {}", e);
                 }
         },
         notify::Config::default(),
@@ -130,8 +134,10 @@ async fn file_watch(tx: Sender<Result<Event, anyhow::Error>>, opts: Cli) -> anyh
     let path = Path::new(&opts.filename);
     watcher.watch(path, notify::RecursiveMode::Recursive)?;
     while let Some(evt) = watch_rx.recv().await {
+        tracing::trace!("got event of type {:?}", evt);
         let file_evt = evt?;
-        if let EventKind::Modify(_) = file_evt.kind {
+        if EventKind::Modify(ModifyKind::Metadata(MetadataKind::Any)) == file_evt.kind ||
+        EventKind::Modify(ModifyKind::Data(DataChange::Any)) == file_evt.kind {
             let path = file_evt.paths[0].clone();
             let monitor_path = path.to_string_lossy().to_string();
             debug!{%monitor_path, "updating file"};
@@ -144,15 +150,14 @@ async fn file_watch(tx: Sender<Result<Event, anyhow::Error>>, opts: Cli) -> anyh
 
 /// turn a filepath into a complete SSE event from parsed markdown
 fn read_to_event<P: AsRef<Path>>(filepath: P) -> Result<Event, anyhow::Error>{
-    let md = read_to_string(filepath)?;
+    let md = read_to_string(&filepath).context(format!("error reading path {}", filepath.as_ref().to_string_lossy()))?;
     let res_html = match markdown::to_html_with_options(&md, &Options::gfm()){
         Ok(h) => h,
         Err(m) => {
-            tracing::error!{%m.source, %m.reason, "error rendering markdown"}
+            error!{%m.source, %m.reason, "error rendering markdown"}
             m.to_string()
         }
     };
-    //let res_html = markdown::to_html(&md);
     Ok(Event::default().data(res_html).event("body"))
 
 }
